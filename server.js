db5f7275f298fc2bcb83bc5b72f223fb09715b0d
EthanWu <wlj0427@gmail.com>
2015-09-08 14:59:20 +0800

    Product page update; static server added.

---
+++ server.js
@@ -0,0 +1,38 @@
+
+var PORT = 8000;
+var http = require('http');
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+    var pathname =request.url;
+    var realpath = '.' + url.parse(request.url).pathname;
+    path.exists(realpath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + realpath + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realpath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+
+                    response.end(err);
+                } else {
+
+                    response.write(file, "binary");
+
+                    response.end();
+                }
+            });
+        }
+    });
+});
+server.listen(PORT);
+console.log('node-static running at http://localhost:%d', PORT);
