b6633d821c5b943bfdf4af1636823a3210e8f336
EthanWu <wlj0427@gmail.com>
2015-10-19 18:10:41 +0800

    产品页优化。

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var PORT = 8000;
 var http = require('http');
 var path = require('path');
@@ -6,27 +5,13 @@ var url = require('url');
 var fs = require('fs');
 
 var server = http.createServer(function (request, response) {
-    var pathname =request.url;
+    var pathname = request.url;
     var realpath = '.' + url.parse(request.url).pathname;
-    path.exists(realpath, function (exists) {
-        if (!exists) {
-            response.writeHead(404, {
-                'Content-Type': 'text/plain'
-            });
-
-            response.write("This request URL " + realpath + " was not found on this server.");
-            response.end();
-        } else {
-            fs.readFile(realpath, "binary", function (err, file) {
-                if (err) {
-                    response.end(err);
-                } else {
-                    response.write(file, "binary");
-                    response.end();
-                }
-            });
-        }
+    fs.readFile(realpath, "binary", function (err, file) {
+        response.write(file, "binary");
+        response.end();
     });
+
 });
 server.listen(PORT);
 console.log('node-static running at http://localhost:%d', PORT);
